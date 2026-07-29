3be2c4ac393d7c813ad06d1689df9c099608f913
yoohyun0830-ship-it <yoohyun0830@gmail.com>
2026-07-29 13:44:47 +0900

    day024h

---
+++ src/day02/Exam2.java
@@ -0,0 +1,32 @@
+package day02;
+public class Exam2 {
+    // == 다형성 ==
+    // [1] 자동 (묵시적) 타입 변환 : byte -> short -> int -> long -> float -> double,
+    byte bytevalue = 100;               // byte 타입에 bytevalue 변수명으로 100 리터럴 대입했다.
+    short shortvalue = bytevalue;       // [기능] byte -> short [*자료유지, 타입변경]
+    int intvalue = shortvalue;          // [기능] short -> int [*자료유지, 타입변경]
+    long longvalue =  intvalue;         // [기능] int -> long [*자료유지, 타입변경]
+    float floatvalue = longvalue;       // [기능] long -> float [*자료유지, 타입변경]
+    double doublevalue =  floatvalue;   // [기능] float -> double [*자료유지, 타입변경]
+
+    // * 연산(계산) 중에 자동 타입변환 , byte/short 연산결과 => int() , 두 항 중에서 더 큰 타입으로 결과 타입 반환
+    byte b1 = 10; byte b2 = 20; short s1 = 30; int i1 = 40; long l1 = 50L; float f1 =60.1f; double d1 = 70.2;
+    int result1 = b1 + b2; // byte + byte =>int
+    int result2 = b2 + s1; // byte + short => int
+    int result3 = i1 + i1; // int + int => int
+    long result4 = i1 + l1; // int +long => long (더큰 타입으로 결과 타입 반환)
+    float result5 = i1 + f1; // int + float => float
+    double result6 = i1 + d1; // int + double => double
+
+    // [2] 강제(명시적) 타입 반환 : byte <- short <- int <- long <- float <- double , *자료손실*
+    double dvalue = 3.14;
+    float fvalue = (float)dvalue;           // [불가능], 자료앞에(변환할타입명) 명시
+    long lvalue = (long)fvalue;             // 3.14 -> 3 손실 발생
+    int ivalue = (int)lvalue;
+    byte bvalue = (byte)ivalue;
+
+
+
+
+    
+}
---
+++ src/day02/Practice1.java
@@ -0,0 +1,55 @@
+package day02;
+
+import java.util.Scanner;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        
+        // 1.
+        byte a =10; int b = 200; long result1 = a+b; // byte+int=> int , int -> long
+        
+        // 2. (변환할자료명) 자료/변수명
+        double z = 3.141592;    int result2 = (int)z; // 3.141592 -> 3
+        System.out.println(result2);
+
+        // 3. 입력준비 : 1)입력객체 2)입력함수
+        Scanner scan = new Scanner(System.in);
+        System.out.print("키를 입력하세요."); double 키 = scan.nextDouble();
+        System.out.println("당신의 키는 약 "+ (int)키 + "cm 이군요.");
+
+        // 4. int+int+int/double => double 입력객체는 클래스내 1개만 존재해도 입력함수 사용가능!
+        System.out.print("1] 정수: "); int 정수1 = scan.nextInt();
+        System.out.print("2] 정수: "); int 정수2 = scan.nextInt();
+        System.out.print("3] 정수: "); int 정수3 = scan.nextInt();
+        double result4 = (정수1 + 정수2 + 정수3) /3.0; // 3으로 나누면 소수점 사라진다. 3.0으로 하여금 자동 double 타입변환
+        System.out.println(result4);
+    }
+}
+    
+/*문제 1: a 와 b 두 변수의 합을 long 타입의 변수에 저장하여 그 결과를 출력하시오.
+선언 코드 :
+byte a = 10;
+int b = 200;
+출력 예시:
+결과: 210 */
+
+/*문제 2: 실수 3.141592를 double 타입 변수에 저장한 뒤, 이 변수를 int 타입으로 강제 변환하여 정수 부분만 출력하시오.
+선언 코드 :
+double z = 3.141592;
+출력 예시:
+원본 double 값: 3.141592
+변환된 int 값: 3 */
+
+/*문제 3: Scanner를 이용해 사용자의 키를 double 타입(예: 178.5)으로 입력받은 후, 이 값을 int 타입으로 강제 변환하여 "당신의 키는 약 OOOcm 이군요." 라고 정수 부분만 출력하시오.
+입력 예시:
+키를 입력하세요: 178.5
+출력 예시:
+당신의 키는 약 178cm 이군요. */
+
+/*문제 4: Scanner를 이용해 서로 다른 3개의 정수를 입력받아, 이 값들의 합을 3으로 나눈 평균을 구하시오. 단, 결과는 소수점이 포함되도록 출력되어야 한다.
+입력 예시:
+첫 번째 정수 입력: 11
+두 번째 정수 입력: 21
+세 번째 정수 입력: 21
+출력 예시:
+평균 결과: 17.666666666666668 */
