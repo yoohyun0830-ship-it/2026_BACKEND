07013cdbeb773664cbf3f0d82a8c9762b193491e
yoohyun0830-ship-it <yoohyun0830@gmail.com>
2026-08-04 17:06:48 +0900

    SQL practice01

--- bin/DATABASE/day01.sql
+++ bin/DATABASE/day01.sql
@@ -10,10 +10,10 @@
 SHOW DATABASE; -- 연결된 DB서버내 모든 데이터베이스 목록 확인
 
 -- [2] 데이터베이스 로컬 경로
-SHOW DATABASE LIKE 'datair' 
+SHOW DATABASE LIKE 'datair'; 
 
 -- [3] 데이터베이스 생성 , 주의할점 : 중복된 데이터베이스 불가능, SQL문법은 대소문자 구분하지 않는다.
-CREATE DATABASE mydb0804 --  연결된 DB서버내 특정한 데이터베이스 생성
+CREATE DATABASE mydb0804;--  연결된 DB서버내 특정한 데이터베이스 생성
 
 -- [4] 데이터베이스 삭제 , if exist 존재하면삭제 주의할점 : 없는 데이터베이스명 삭제 불가능
 DROP DATABASE mydb0804;
